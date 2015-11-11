c0e03f3b2d7d341de921d2277e3bf57ba1d7d303
tejagrandhi <srikrishnateja.grandhi@gmail.com>
2015-11-12 00:19:11 +0530

    Submit Commit

--- src/removeArrayDuplicates.cpp
+++ src/removeArrayDuplicates.cpp
@@ -12,8 +12,40 @@ NOTES: Don't create new array, try to change the input array.
 */
 
 #include <stdio.h>
-
+#include<malloc.h>
 void * removeArrayDuplicates(int *Arr, int len)
 {
-	return NULL;
+	
+	int Reference[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, count = 0, index, value = 0;
+	if (Arr == NULL || len < 0)
+		return NULL;
+	for (index = 0; index < len; index++)
+	{
+		if (Reference[Arr[index]] == 0)
+		{
+			count++;
+			Reference[Arr[index]] = 1;
+		}
+	}
+	if (count == len)
+	{
+		return Arr;
+	}
+	else
+	{
+		for (index = 0; index < 10; index++)
+		{
+			if (Reference[index] == 1)
+			{
+				Arr[value++] = index;
+			}
+			if (value == count)
+			{
+				break;
+			}
+		}
+		// Arr = (int*)realloc(Arr, count*sizeof(int));
+		return Arr;
+	}
+	
 }
